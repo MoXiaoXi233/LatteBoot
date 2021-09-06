07f5c008e71ce215749145d43dee38cf5b8152e4
Tim Keddnyo <65981689+AndyER03@users.noreply.github.com>
2021-09-06 17:08:28 +0300

    Initial add

---
+++ app/src/main/java/com/andyer03/latteboot/AppWidgetProvider.kt
@@ -0,0 +1,29 @@
+package com.andyer03.latteboot
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.app.PendingIntent
+import android.widget.RemoteViews
+
+class WindowsWidget : AppWidgetProvider() {
+
+    override fun onEnabled(context: Context) {
+        super.onEnabled(context)
+    }
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        for (appWidgetId in appWidgetIds) {
+            val intent = Intent(context, MainActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+            val views = RemoteViews(context.packageName, R.layout.windows)
+            views.setOnClickPendingIntent(R.id.windows, pendingIntent)
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
